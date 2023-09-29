6ec33ef45f93ce739a4a6eafa45d958a64222952
DanKe <1586017466@qq.com>
2023-09-29 11:12:04 +0800

    1.0

--- Steam Preferences Fixer.user.js
+++ Steam Preferences Fixer.user.js
@@ -7,7 +7,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // 获取所有 class 为 "preference_row account_setting_not_customer_facing" 的元素
@@ -17,7 +17,7 @@
     const label1 = element1.querySelector(".store_pref_desc label.account_manage_checkbox");
     label1.textContent = "频繁的裸露画面或色情内容";
     const span1 = element1.querySelector(".store_pref_desc span.account_setting_parenthetical")
-    span1.innerHTML = '主要展示裸露画面或色情主题的游戏或内容。勾选此复选框即表示您确认自己已至少年满18周岁。<br><a href="#">** 国区隐藏选项 勾选后不显示已勾选**</a>';
+    span1.innerHTML = '主要展示裸露画面或色情主题的游戏或内容。勾选此复选框即表示您确认自己已至少年满18周岁。<br><a href="https://github.com/DanKE123abc/Steam-Preferences-Fixer">** 国区隐藏选项 勾选后不显示已勾选**</a>';
     element1.classList.remove("account_setting_not_customer_facing");
     element1.classList.add("preference_row");
 
@@ -26,7 +26,7 @@
     const label2 = element2.querySelector(".store_pref_desc label.account_manage_checkbox");
     label2.textContent = "仅限成人的色情内容";
     const span2 = element2.querySelector(".store_pref_desc span.account_setting_parenthetical")
-    span2.innerHTML = '包含仅针对成人受众的性意味明显或露骨的游戏或内容。勾选此复选框即表示您确认自己已至少年满18周岁。<br><a href="#">** 国区隐藏选项 勾选后不显示已勾选**</a>';
+    span2.innerHTML = '包含仅针对成人受众的性意味明显或露骨的游戏或内容。勾选此复选框即表示您确认自己已至少年满18周岁。<br><a href="https://github.com/DanKE123abc/Steam-Preferences-Fixer">** 国区隐藏选项 勾选后不显示已勾选**</a>';
     element2.classList.remove("account_setting_not_customer_facing");
     element2.classList.add("preference_row");
 
